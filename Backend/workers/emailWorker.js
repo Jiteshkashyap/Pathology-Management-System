c2a5d38079cd85c3e3673d9ff726729b84dbb46e
Jiteshkashyap <94862516+Jiteshkashyap@users.noreply.github.com>
2026-02-27 16:03:37 +0530

    Cloud

--- Backend/workers/emailWorker.js
+++ Backend/workers/emailWorker.js
@@ -12,12 +12,15 @@ export const startEmailWorker= async()=>{
     console.log("Email data:", data);
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+       host: "smtp.gmail.com",
+       port: 587,
+       secure: false,
+       family: 4,
+       auth: {
+         user: process.env.EMAIL_USER,
+         pass: process.env.EMAIL_PASS,
+  },
+});
 
     console.log("📧 Sending email...");
 
